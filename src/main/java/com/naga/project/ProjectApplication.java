4384001a24ae954a79417a316fe9b9abcf8d658f
hoyeol0513 <hoyeol0513@naver.com>
2022-10-21 10:20:56 +0900

    sendMail 로직 send함수 추가

--- src/main/java/com/naga/project/ProjectApplication.java
+++ src/main/java/com/naga/project/ProjectApplication.java
@@ -2,8 +2,10 @@ package com.naga.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ProjectApplication {
 
 	public static void main(String[] args) {
